b4534040df887e17aa32b3adf421cc5d178bc471
ykaeber <y.kaeber@posteo.de>
2024-02-09 13:47:51 +0100

    data for max to test FINN

--- tba.R
+++ tba.R
@@ -1,4 +1,5 @@
-
+library(data.table)
+library(ggplot2)
 
 envMfunctions = list(
   f1 = function(par, env) {
@@ -93,7 +94,6 @@ runModel = function(envM,
   
   results = vector("list", timesteps+1)
   results[[1]] = cohortMat
-  tstep=1
   for(tstep in 1:timesteps) {
     if(nrow(cohortMat) > 0){
       g = growthF(cohortMat, tstep, parGrowth)
@@ -115,44 +115,43 @@ runModel = function(envM,
   }
   return(results)
 }
-cohortMat = cbind(ID = 1:10, 
-                  dbh = runif(10, 30, 100), 
-                  nTree = rpois(10, 10), 
-                  Species = sample.int(5, 5, replace = TRUE))
-
-## envM
-# matrix, row = Zeit, columns = Predictors
-envM = matrix(rnorm(500*2), 500, 2)
-
-# Parameter
-parGlobal = matrix(runif(5), 5, 1) 
-parReg = matrix(runif(5*3,0, 3), 5, 3)
-parMort = matrix(runif(5*4),5,4)
-parMort[,4] = runif(5, 250, 350)
-parGrowth = matrix(runif(5*4, 0, 7), 5, 4)
-
-res = runModel(envM = envM, 
-               envMfunctions = envMfunctions, 
-               cohortMat = cohortMat,
-               compF = compF, 
-               stateF = stateF,
-               growthF = growthF,
-               mortF = mortF,
-               regF = regF,
-               parGlobal = parGlobal,
-               parReg = parReg,
-               parMort = parMort,
-               parGrowth = parGrowth, timesteps = 500L)
-
-
-library(data.table)
-library(ggplot2)
-out_l = lapply(1:length(res[-1]), function(i) data.table(res[-1][[i]], timestep = i))
-out_dt = rbindlist(out_l[-1])
-circle_area <- function(d) pi * (d/100 / 2)^2
-out_dt[,ba := circle_area(dbh)*nTree,]
-ggplot(out_dt, aes(x = timestep, y = ba, color = factor(Species)))+
-  geom_line()
+# cohortMat = cbind(ID = 1:10, 
+#                   dbh = runif(10, 30, 100), 
+#                   nTree = rpois(10, 10), 
+#                   Species = sample.int(5, 5, replace = TRUE))
+# 
+# ## envM
+# # matrix, row = Zeit, columns = Predictors
+# envM = matrix(rnorm(500*2), 500, 2)
+# 
+# # Parameter
+# parGlobal = matrix(runif(5), 5, 1) 
+# parReg = matrix(runif(5*3,0, 3), 5, 3)
+# parMort = matrix(runif(5*4),5,4)
+# parMort[,4] = runif(5, 250, 350)
+# parGrowth = matrix(runif(5*4, 0, 7), 5, 4)
+# 
+# res = runModel(envM = envM, 
+#                envMfunctions = envMfunctions, 
+#                cohortMat = cohortMat,
+#                compF = compF, 
+#                stateF = stateF,
+#                growthF = growthF,
+#                mortF = mortF,
+#                regF = regF,
+#                parGlobal = parGlobal,
+#                parReg = parReg,
+#                parMort = parMort,
+#                parGrowth = parGrowth, timesteps = 500L)
+# 
+# 
+# 
+# out_l = lapply(1:length(res[-1]), function(i) data.table(res[-1][[i]], timestep = i))
+# out_dt = rbindlist(out_l[-1])
+# circle_area <- function(d) pi * (d/100 / 2)^2
+# out_dt[,ba := circle_area(dbh)*nTree,]
+# ggplot(out_dt, aes(x = timestep, y = ba, color = factor(Species)))+
+#   geom_line()
 
 Nspecies = 4
 
@@ -165,7 +164,7 @@ Nspecies = 4
 species_dt = data.table(
   names = c("Fagus sylvatica", "Picea abies", "Acer pseudoplatanus", "Betula pendula", "Abies alba"),
   dbh2height = c(0.6,0.6,0.6,0.6,0.6),
-  Light = c(0.1,0.15,0.3,0.4,0.07),
+  Light = c(0.1,0.15,0.5,0.6,0.07),
   Temp = c(0.6,0.3,0.7,0.4,0.3),
   Prec = c(0.6,0.7,0.4,0.4,0.5),
   maxD = c(350,300,200,100,350),
@@ -185,41 +184,96 @@ cohortMat = cbind(ID = 1:10,
                   Species = sample.int(5, 5, replace = TRUE))
 
 out_dt_all  <- data.table()
+env_out_all  <- data.table()
+Nsites = 1000
+Ntimesteps = 300
+Npatches = 100
+NsamplesPerEnv = ceiling(sqrt(Nsites))
+EnvSiteAvg = seq(0,1,length.out = NsamplesPerEnv)
+EnvSiteAvgM = expand.grid(list(EnvSiteAvg,EnvSiteAvg))
+cat("start at")
+Sys.time()
 time = system.time(
   {
-    for(p in 1:50){
-      envM = matrix(c(rnorm(500, 0.6, 0.1),rnorm(500,0.55,0.1)), 500, 2)
-      
-      res2 = runModel(envM = envM, 
-                      envMfunctions = envMfunctions, 
-                      cohortMat = cohortMat,
-                      compF = compF, 
-                      stateF = stateF,
-                      growthF = growthF,
-                      mortF = mortF,
-                      regF = regF,
-                      parGlobal = parGlobal,
-                      parReg = parReg,
-                      parMort = parMort,
-                      parGrowth = parGrowth, timesteps = 500)
-      
-      out_l = lapply(1:length(res2[-1]), function(i) data.table(res2[-1][[i]], timestep = i))
-      out_dt = rbindlist(out_l)
-      out_dt$patch = p
-      out_dt_all = rbind(out_dt_all, out_dt)
+    for(i_site in 1:Nsites){
+      envAvg1 = EnvSiteAvgM[i_site,1]
+      envAvg2 = EnvSiteAvgM[i_site,2]
+      rm(envM)
+      envM = matrix(c(rnorm(Ntimesteps, envAvg1, 0.1),rnorm(Ntimesteps,envAvg2,0.1)), Ntimesteps, 2)
+      envM[envM > 1] = 1
+      envM[envM < 0] = 0
+      env_out = data.table(
+        timestep = 1:Ntimesteps,
+        site = i_site,
+        envAct1 = envM[,1],
+        envAct2 = envM[,2],
+        envAvg1 = envAvg1,
+        envAvg2 = envAvg2
+        )
+      if(i_site == 1 & p == 1) fwrite(env_out, file = "evaluation3-env.csv", append = F) else fwrite(env_out, file = "evaluation3-env.csv", append = T)
+      env_out_all = rbind(env_out_all, env_out)
+      for(p in 1:Npatches){
+        cat(paste0("\r running site ", i_site, "/", Nsites, " patch ", p, "/", Npatches, "          "))
+        res2 = runModel(envM = envM, 
+                        envMfunctions = envMfunctions, 
+                        cohortMat = cohortMat,
+                        compF = compF, 
+                        stateF = stateF,
+                        growthF = growthF,
+                        mortF = mortF,
+                        regF = regF,
+                        parGlobal = parGlobal,
+                        parReg = parReg,
+                        parMort = parMort,
+                        parGrowth = parGrowth, timesteps = Ntimesteps)
+        
+        out_l = lapply(1:length(res2[-1]), function(i) data.table(res2[-1][[i]], timestep = i))
+        out_dt = rbindlist(out_l)
+        out_dt$patch = p
+        out_dt$site = i_site
+        if(i_site == 1 & p == 1) fwrite(out_dt, file = "evaluation3-obs.csv", append = F) else fwrite(out_dt, file = "evaluation3-obs.csv", append = T)
+        # out_dt_all = rbind(out_dt_all, out_dt)
+      }
     }
   })
 time
 
+out_dt_all <- fread("evaluation2-obs.csv")
+env_out_all <- fread("evaluation2-env.csv")
+
+out_dt_all <- merge(out_dt_all, env_out_all, by = c("site", "timestep"))
+
+ggplot(env_out_all, aes(x = factor(envAvg1), y = envAct1))+
+  geom_boxplot()
+ggplot(env_out_all, aes(x = factor(envAvg2), y = envAct2))+
+  geom_boxplot()
+
+# fwrite(out_dt_all, "evaluation-obs.csv")
+# fwrite(env_out_all, "evaluation-env.csv")
+# fwrite(envM, "evaluation-env.csv")
+
 circle_area <- function(d) pi * (d/100 / 2)^2
 out_dt_all[,ba := (circle_area(dbh)*nTree)/0.1,]
-out_dt_all[,SpeciesName := factor(Species, levels = 1:5, labels = species_dt$names),]
-out_dt_all2 <- out_dt_all[,.(ba = sum(ba)), by = .(timestep,SpeciesName,patch)]
+out_dt_all[,SpeciesName := factor(Species, levels = 1:5, labels = paste(1:5, species_dt$names)),]
+out_dt_all2 <- out_dt_all[,.(
+  ba = sum(ba),
+  nTree = sum(nTree)
+  ), by = .(site,timestep,SpeciesName,patch, envAvg1, envAvg2)]
 
-ggplot(out_dt_all2[,.(ba = mean(ba)), by = .(timestep,SpeciesName)], aes(x = timestep, y = ba, color = SpeciesName))+
+p1 <- ggplot(out_dt_all2[,.(
+  ba = mean(ba)
+  ), by = .(site,timestep,SpeciesName,envAvg1,envAvg2)], aes(x = timestep, y = ba, color = SpeciesName))+
+  geom_line()+
+  facet_grid(round(envAvg1,2)~round(envAvg2,2))
+p1
+p2 <- ggplot(out_dt_all2[,.(
+  nTree = sum(nTree)
+  ), by = .(timestep,SpeciesName)], aes(x = timestep, y = nTree, color = SpeciesName))+
   geom_line()
 
-ggplot(out_dt_all[patch == 1,.(timestep,SpeciesName,dbh)], aes(x = cut(timestep, breaks = 10), y = dbh, color = SpeciesName))+
+ggpubr::ggarrange(p1,p2,ncol = 1)
+
+ggplot(out_dt_all[patch == 1,.(timestep,SpeciesName,dbh)], aes(x = cut(timestep, breaks = 10), y = dbh, color = paste(SpeciesName)))+
   geom_boxplot()
 
 View(out_dt_all[patch == 1,.(timestep,SpeciesName,dbh)])
